8129616e9990dd552edd9b5fde25493283609804
Jason Cobb <jason.e.cobb@gmail.com>
2020-07-08 21:18:45 -0400

    Add tournament_change agent

--- rulekeep/history.py
+++ rulekeep/history.py
@@ -155,6 +155,9 @@ def agent_string(data_path, agent):
     try: return "initiation of " + agent["tournament_init"]["name"] + " by " + agent["tournament_init"]["initiator"]
     except KeyError: pass
 
+    try: return agent["tournament_change"]["person"] + " as part of " + agent["tournament_change"]["name"]
+    except KeyError: pass
+
     try: return agent["person"]
     except KeyError: pass
 
