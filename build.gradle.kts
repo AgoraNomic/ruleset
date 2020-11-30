7a73c3ed29a0ed774aab2603cdb75d2cd2c0383d
Jason Cobb <jason.e.cobb@gmail.com>
2020-11-30 16:54:36 -0500

    Add kotlinx.serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    kotlin("plugin.serialization") version "1.4.20"
 }
 
 group = "org.agoranomic"
