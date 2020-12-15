7ba049ea3d3b4221c0f005f1e40cef3a55bc42bc
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-14 20:03:00 -0500

    Add HistoricalEntry

---
+++ src/main/kotlin/org/agoranomic/ruleset/history/Entry.kt
@@ -0,0 +1,15 @@
+package org.agoranomic.ruleset.history
+
+import java.time.LocalDate
+
+sealed class HistoricalDate {
+    data class Known(val date: LocalDate) : HistoricalDate()
+    data class Around(val date: LocalDate) : HistoricalDate()
+    data class Between(val start: LocalDate, val end: LocalDate) : HistoricalDate()
+}
+
+data class HistoricalEntry(
+    val change: HistoricalChange,
+    val cause: HistoricalCause,
+    val date: HistoricalDate,
+)
