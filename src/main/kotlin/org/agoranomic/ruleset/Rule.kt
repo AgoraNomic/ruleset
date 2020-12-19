c3c233825fab21dcc8a1efa126a9ad957c94e666
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-19 00:01:51 -0500

    Split Model.kt into more logical groupings

---
+++ src/main/kotlin/org/agoranomic/ruleset/Rule.kt
@@ -0,0 +1,61 @@
+package org.agoranomic.ruleset
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.toImmutableMap
+import org.agoranomic.ruleset.history.HistoricalEntry
+import java.math.BigDecimal
+import java.math.BigInteger
+
+data class RuleHistory(val entries: ImmutableList<HistoricalEntry>) {
+    // TODO: enforce constraints
+    constructor(entries: List<HistoricalEntry>) : this(entries.toImmutableList())
+}
+
+inline class RuleNumber(val raw: BigInteger) : Comparable<RuleNumber> {
+    override fun compareTo(other: RuleNumber): Int {
+        return (this.raw).compareTo(other.raw)
+    }
+
+    override fun toString(): String {
+        return raw.toString()
+    }
+}
+
+data class RuleState(
+    val id: RuleNumber,
+    val title: String,
+    val power: BigDecimal,
+    val text: String,
+    val history: RuleHistory,
+    val annotations: RuleAnnotations?,
+)
+
+data class RulesetState(private val rulesByNumber: ImmutableMap<RuleNumber, RuleState>) : Iterable<RuleState> {
+    init {
+        require(rulesByNumber.all { it.key == it.value.id })
+    }
+
+    constructor(rulesByNumber: Map<RuleNumber, RuleState>) : this(rulesByNumber.toImmutableMap())
+
+    companion object {
+        fun from(collection: Collection<RuleState>): RulesetState {
+            return RulesetState(collection.associateByPrimaryKey { it.id })
+        }
+    }
+
+    override fun iterator(): Iterator<RuleState> {
+        return rulesByNumber.values.iterator()
+    }
+
+    val ruleNumbers get() = rulesByNumber.keys
+
+    fun ruleByNumber(id: RuleNumber): RuleState {
+        return rulesByNumber.getValue(id)
+    }
+
+    fun rulesByNumbers(ids: Collection<RuleNumber>): RulesetState {
+        return from(ids.map { ruleByNumber(it) })
+    }
+}
