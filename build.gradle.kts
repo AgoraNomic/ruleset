d32227b5dd18d8ddf11afab453603d936f105d5c
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-22 00:55:05 -0500

    Add application plugin to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    application
 }
 
 group = "org.agoranomic"
@@ -20,6 +21,10 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+application {
+    mainClassName = "org.agoranomic.ruleset.CliKt"
 }
 
 tasks.test {
