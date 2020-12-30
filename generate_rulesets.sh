6da213a44f710d006d224f9e70d33660982cd19b
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-29 20:13:15 -0500

    Add generate_rulesets.sh

---
+++ generate_rulesets.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pushd . > /dev/null
+
+OUT_DIR="$(realpath -- generated)"
+SCRIPT_DIR="$(realpath -- "$(dirname -- "$0")")"
+cd -- "$SCRIPT_DIR"
+
+./gradlew install
+
+RULEKEEPOR_EXEC="$SCRIPT_DIR/build/install/rulekeepor/bin/rulekeepor"
+RULES_DATA_DIR="$SCRIPT_DIR/rules_data"
+REGS_DATA_DIR="$SCRIPT_DIR/regs_data"
+
+mkdir -p -- "$OUT_DIR"
+
+"$RULEKEEPOR_EXEC" \
+  --template-file "$REGS_DATA_DIR/config/slr_format" \
+  --index-file "$RULES_DATA_DIR/config/index" \
+  --proposals-dir "$RULES_DATA_DIR/proposals" \
+  --rules-dir "$RULES_DATA_DIR/rules" \
+  --header-file "$RULES_DATA_DIR/config/header" \
+  --out-file "$OUT_DIR/slr.txt" \
+  --no-history \
+  --no-annotations \
+  --exclude-rule-files 2540,2541,2558
+
+"$RULEKEEPOR_EXEC" \
+  --template-file "$RULES_DATA_DIR/config/flr_format" \
+  --index-file "$RULES_DATA_DIR/config/index" \
+  --proposals-dir "$RULES_DATA_DIR/proposals" \
+  --rules-dir "$RULES_DATA_DIR/rules" \
+  --header-file "$RULES_DATA_DIR/config/header" \
+  --out-file "$OUT_DIR/flr.txt" \
+  --history \
+  --annotations \
+  --exclude-rule-files 2540,2541,2558
+
+"$RULEKEEPOR_EXEC" \
+  --template-file "$REGS_DATA_DIR/config/flr_format" \
+  --index-file "$REGS_DATA_DIR/config/index" \
+  --rules-dir "$REGS_DATA_DIR/rules" \
+  --header-file "$REGS_DATA_DIR/config/header" \
+  --out-file "$OUT_DIR/acorn.txt" \
+  --history \
+  --annotations \
+  --entity-kind "Regulation"
+
+popd > /dev/null
