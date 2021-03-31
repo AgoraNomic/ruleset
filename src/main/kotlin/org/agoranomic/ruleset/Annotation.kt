c8e7d74c6b1ed414e7a328fddcd7b0668003d46f
Jason Cobb <jason.e.cobb@gmail.com>
2021-03-31 19:13:34 -0400

    Use delegation in RuleAnnotations

--- src/main/kotlin/org/agoranomic/ruleset/Annotation.kt
+++ src/main/kotlin/org/agoranomic/ruleset/Annotation.kt
@@ -40,25 +40,6 @@ data class HistoricalCfjAnnotation(
     )
 }
 
-data class RuleAnnotations(val annotations: ImmutableList<RuleAnnotation>) : Collection<RuleAnnotation> {
+data class RuleAnnotations(val annotations: ImmutableList<RuleAnnotation>) : Collection<RuleAnnotation> by annotations {
     constructor(annotations: List<RuleAnnotation>) : this(annotations.toImmutableList())
-
-    override val size: Int
-        get() = annotations.size
-
-    override fun contains(element: RuleAnnotation): Boolean {
-        return annotations.contains(element)
-    }
-
-    override fun containsAll(elements: Collection<RuleAnnotation>): Boolean {
-        return annotations.containsAll(elements)
-    }
-
-    override fun isEmpty(): Boolean {
-        return annotations.isEmpty()
-    }
-
-    override fun iterator(): Iterator<RuleAnnotation> {
-        return annotations.iterator()
-    }
 }
